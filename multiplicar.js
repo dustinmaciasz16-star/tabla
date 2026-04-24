68a9be26ede0c265daaf62f007c4f663d52f8e0c
dustinmaciasz16-star <dustinmaciasz16@gmail.com>
2026-04-24 00:21:37 -0500

    mejora de errores

--- multiplicar.js
+++ multiplicar.js
@@ -4,6 +4,7 @@ function generarTabla(){
     let numero = document.getElementById("numero").value;
     let contenedor = document.getElementById("tabla");
     let mensaje = document.getElementById("mensaje");
+    let contenido = "";
 
     if(numero === ""){
         mensaje.textContent = "⚠ Por favor, ingresa un número.";
@@ -19,14 +20,29 @@ function generarTabla(){
         return;
     }
 
-    let contenido = "";
+    if(numero % 1 !== 0){
+        mensaje.textContent = "⚠ Ingresa un número que no sea decimal.";
+        mensaje.className = "mensaje error";
+        contenedor.innerHTML = "";
+        return;
+    }
+
+    if(numero >= 12){
+        mensaje.textContent = "solo se puede hasta la tabla del 12";
+        mensaje.className = "mensaje error";
+        contenedor.innerHTML = "";
+        return;
+    }
+
+
+
 
     for(let i = 1; i <= 10; i++){
-        contenido += `<div class="fila">${numero} x ${i} = ${numero * i}</div>`;
+        contenido += "<div class='fila'>"+ numero +  "x" + i + "=" + (numero * i) + "</div>";
     }
 
     contenedor.innerHTML = contenido;
 
-    mensaje.textContent = `✅ Mostrando la tabla del ${numero}`;
+    mensaje.textContent = "✅ Mostrando la tabla del" + numero;
     mensaje.className = "mensaje ok";
 }
